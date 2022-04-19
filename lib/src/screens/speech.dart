db1c051e07d0cb6b41d35e5d7f8e55bd4bda8f65
*D8uZD3ENJO <shirakura@colors.jp>
2022-04-19 22:33:33 +0900

    master

--- lib/src/screens/speech.dart
+++ lib/src/screens/speech.dart
@@ -17,6 +17,8 @@ class SpeechScreen extends StatefulWidget {
 
 class _SpeechScreenState extends State<SpeechScreen> {
   List _items = [];
+  bool _searchBoolean = false; //追加
+  List _searchIndexList = []; //追加
 
   void loadJson() async {
     final String response = await rootBundle.loadString('assets/json/abeshinzo.json');
@@ -52,14 +54,43 @@ class _SpeechScreenState extends State<SpeechScreen> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('議会発言一覧'),
-        toolbarHeight: 40,
+  Widget _searchTextField() { //追加
+    return TextField(
+      autofocus: true, //TextFieldが表示されるときにフォーカスする（キーボードを表示する）
+      cursorColor: Colors.white, //カーソルの色
+      style: TextStyle( //テキストのスタイル
+        color: Colors.white,
+        fontSize: 20,
       ),
-      body: Scrollbar(
+      textInputAction: TextInputAction.search, //キーボードのアクションボタンを指定
+      decoration: InputDecoration( //TextFiledのスタイル
+        enabledBorder: UnderlineInputBorder( //デフォルトのTextFieldの枠線
+          borderSide: BorderSide(color: Colors.white)
+        ),
+        focusedBorder: UnderlineInputBorder( //TextFieldにフォーカス時の枠線
+          borderSide: BorderSide(color: Colors.white)
+        ),
+        hintText: '発言を検索', //何も入力してないときに表示されるテキスト
+        hintStyle: TextStyle( //hintTextのスタイル
+          color: Colors.white60,
+          fontSize: 20,
+        ),
+      ),
+      onChanged: (String s) { //追加
+        setState(() {
+          _searchIndexList = [];
+          for (int i = 0; i < _items.length; i++) {
+            if (_items[i]["Speech"].contains(s)) {
+              _searchIndexList.add(_items[i]);
+            }
+          }
+        });
+      },
+    );
+  }
+
+  Widget _defaultListView() {
+    return Scrollbar(
         child: ListView.builder(
           itemCount: _items.length,
           itemBuilder: (context, index) {
@@ -73,7 +104,61 @@ class _SpeechScreenState extends State<SpeechScreen> {
             );
           },
         )
-      )
+      );
+  }
+
+  Widget _searchListView() {
+    if(_searchIndexList.length == 0) {
+      return Text('結果はありません');
+    } else {
+      return Scrollbar(
+          child: ListView.builder(
+            itemCount: _searchIndexList.length,
+            itemBuilder: (context, index) {
+              return Card(
+                margin: const EdgeInsets.all(10),
+                child: ListTile(
+                  title: Text(_searchIndexList[index]["Speech"], maxLines: 1,),
+                  subtitle: Text(_searchIndexList[index]["Date"]),
+                  onTap: () => {onPress(_searchIndexList[index])},
+                ),
+              );
+            },
+          )
+        );
+      }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: !_searchBoolean ? Text('議会発言一覧') : _searchTextField(),
+        toolbarHeight: 40,
+        actions: !_searchBoolean?
+          [
+            IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              setState(() {
+                _searchBoolean = true;
+                _searchIndexList = []; //追加
+              });
+            })
+          ] 
+          :
+          [
+            IconButton(
+              icon: Icon(Icons.clear),
+              onPressed: () {
+                setState(() {
+                  _searchBoolean = false;
+                });
+              }
+            )
+          ]
+      ),
+      body: !_searchBoolean ? _defaultListView() : _searchListView()
     );
   }
 }
